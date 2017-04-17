d855c2455696ad365edf79665979af2e5da00033
HustonMmmavr <hustonmavr@gmail.com>
2017-04-17 17:09:48 +0300

    added proces exceptions

--- ask/views.py
+++ ask/views.py
@@ -1,6 +1,6 @@
 #from cgi import parse_qsl
 # from django import http
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from  django.template import loader
 from django.template import Template
 from django.shortcuts import render, render_to_response
@@ -50,15 +50,22 @@ def settings(request):
 #and send url to paginator
 
 
-def paginate(objects, count_on_page, num_page, pages_to_show):
-#	try
-	interval = int(pages_to_show / 2) + 1
+def paginate(objects, count_on_page, page_num, pages_to_show):
 	paginator = Paginator(objects, count_on_page)
+	interval = int(pages_to_show / 2) + 1
+	
+	try:
+		num_page = int(page_num)
+	except PageNotAnInteger:
+		num_page = 1
+
+	if num_page > paginator.num_pages:
+		num_page = paginator.num_pages
+	
+	paginated = paginator.page(num_page)
 	min_idx = 0 if (int(num_page) - interval < 0) else int(num_page) - interval
 	max_idx =  num_page + interval if (num_page + interval < paginator.num_pages) else paginator.num_pages
-	paginated = paginator.page(int(num_page))
 	page_range = paginator.page_range[min_idx:max_idx]
-#	exept PageNotInteger:
 	return paginated, page_range
 
 
@@ -68,121 +75,22 @@ def hot(request, page_num=1):
 	return render(request, 'hot.html', {'questions': questions, 'page_range': page_range, 'paginator_url': 'hot-url'}) 
 
 def tag(request, tag_name, page_num=1):
-	tag = Tag.objects.get_by_title(tag_name)
+	try:
+		tag = Tag.objects.get_by_title(tag_name)
+	except Tag.DoesNotExist:
+		raise Http404()
 	questionsObjs = Question.objects.list_tag(tag)
 	questions, page_range = paginate(questionsObjs, 5, int(page_num), 5)
 	return render(request, 'tag.html', {'questions': questions, 'page_range': page_range, 'paginator_url': 'tag-url', 'tag': tag}) 
 
 def question(request, question_number):
-	q = Question.objects.get_single(int(question_number))
-	print(q.text)
+	try:
+		q = Question.objects.get_single(int(question_number))
+	except Question.DoesNotExist:
+		raise Http404()
 	return render(request, 'question.html', {'question': q})
 
 def all(request, page_num=1):
 	questions = Question.objects.all()#.append_author()#.append_answers_count()
-	questions, page_range = paginate(questions, 5, int(page_num), 5)
-	print(page_range)
+	questions, page_range = paginate(questions, 5, page_num, 5)
 	return render(request, 'all.html', {'questions': questions, 'page_range': page_range, 'paginator_url': 'all-url'})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		#print(str(q.answers_count) + ' ' + str(q.likes))#owner.first_name)
-	# for i in range(1,30):
-	# 	questions.append({
-	# 		'title': 'title ' + str(i),
-	# 		'id': i,
-	# 		'text': 'text' + str(i),
-	# 		'tags': {'politics', 'america'},
-	# })
-	# 	print()
-
-
-
-
-
-
-
-
-	#temp = loader.get_template('hot.html')
-	#url(r'^/', views.all, name='all-url'),
-
-
-
-
-
-	#temp = loader.get_template('question.html')
-
-
-	#temp = loader.get_template('login.html')
-
-
-#def paginate(request, objects, template, 
-# 	      count_on_page, page_num, context_variables):#name):
-# 	paginator = Paginator(objects, 5)
-# 	questions = paginator.page(int(page_num))	
-# 	return render(request, tamplate, context_variables) 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	#template = 'all.html'
-
-	#return paginat(request, objects, template, 5. page_num, 
-	 #questions = ['john', 'paul', 'george', 'ringo', 'paul', 'jim', 'kim','charles']
-	#tags = { 	
-
-
-#render(request, 'login.html', {'post': post})#HttpResponse(temp.render(context))#, env))
-#	#resp = ['login']
-#	#return HttpResponse(resp)
-
-#def login(wsgi_request):
-#	resp = ['login']
-#	return HttpResponse(resp)
-
- #item.join(' '))
-        #resp.append(wsgi_request.GET.dict)
-        # resp.append("aaa")
-        # print (wsgi_request.GET)
-    # if HttpRequest.GET
-        # resp.append(HttpRequest.GET)
-        # print HttpRequest.GET
-
-    #resp_len = sum(len(line) for line in resp)
-#    response('200 OK', [('Content-type', 'text/html'),
-#                              ('Content-Length', str(resp_len))])
-
-    # xrange
-    #for i in range(len(resp)):
-    #    resp[i] = bytes(resp[i], "utf-8")
